4a229c21ec4fd40602cd90f9d279f3d3218dea0e
RafOs14 <81370636+RafOs14@users.noreply.github.com>
2021-11-20 12:22:33 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import Aurora from './components/Aurora';
+import Beach from './components/Beach';
+import Forest from './components/Forest';
+import Jungle from './components/Jungle';
+import Lake from './components/Lake';
+import Milky from './components/Milky';
+import { Navegacion } from './components/Navegacion';
+
+function App() {
+  return (
+    <Router>
+      <div className="container">
+        <h1 className="text-center mt-3 mb-3">
+          Galería de Imágenes
+        </h1>
+      </div>
+      <Route path="/Aurora" component={Aurora}/>
+      <Route path="/Beach" component={Beach}/>
+      <Route path="/Forest" component={Forest}/>
+      <Route path="/Jungle" component={Jungle}/>
+      <Route path="/Lake" component={Lake}/>
+      <Route path="/Milky" component={Milky}/>
+
+      <Navegacion/>
+
+    </Router>
+  );
+}
+
+export default App;
